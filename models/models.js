81e2f9b7c5d6dd81bc07e95470ad4e06064010c0
Sixto Solís <sixtosentido@gmail.com>
2015-06-19 23:47:04 +0200

    Creación de BBDD sqlitey postgres

---
+++ models/models.js
@@ -0,0 +1,56 @@
+var path = require('path');
+
+//POSTGRES DATABASE_URL = postgres://user:passwd@host:post/database
+//SQLITE DATABASE_URL = sqlite://:@:/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+//Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+//Usar BBDD SQLITE o POSTGRES
+
+var sequelize = new Sequelize (DB_name, user, pwd,
+{
+	dialect: protocol,
+	protocol: protocol,
+	port: port,
+	host: host,
+	storage: storage,
+	omitNull: true
+}
+);
+
+//Usar BBDD SQLite
+var sequelize = new Sequelize(null,null,null,
+								{dialect: "sqlite", storage: "quiz.sqlite"}
+	);
+
+//Importar definición de tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+//Exportar definición de tabla Quiz
+exports.Quiz = Quiz;
+
+//sequelize.sync() crea e inicializa la tabla de preguntas en BBDD
+sequelize.sync().success(function(){
+	//success ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(function(count){
+		if (count===0){ //La tabla solo se inicializa si está vacía
+			Quiz.create({
+							pregunta: 'Capital de Italia',
+							respuesta: 'Roma'
+						})
+			.success(function(){console.log('Base de datos inicializada')})
+		}
+		else console.log('Base de datos existente');
+	});
+});
