8985c1c40d72a3593d8946e89751e1bcb27282d1
Sixto Solís <sixtosentido@gmail.com>
2015-06-21 22:07:51 +0200

    Formulario añadir preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -46,3 +46,19 @@ exports.index = function (req,res){
 exports.author = function (req,res){
 	res.render('quizes/author');
 };
+
+// GET /quizes/new
+exports.new = function (req,res){
+	var quiz = models.Quiz.build(
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function (req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -15,5 +15,7 @@ router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/author', quizController.author);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 module.exports = router;
