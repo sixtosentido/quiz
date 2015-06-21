993206d428c79baf32624e1a22a0e301c2737484
Sixto Solís <sixtosentido@gmail.com>
2015-06-21 21:16:22 +0200

    Búsqueda de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -28,7 +28,16 @@ exports.answer = function (req,res){
 
 // GET /quizes
 exports.index = function (req,res){
-	models.Quiz.findAll().then(function(quizes){
+	var search = '%';
+	if (req.query.search){
+		search = '%'+req.query.search+'%';
+		search = search.trim().replace(/\s/g,'%');
+	}
+
+	models.Quiz.findAll({
+		where: ["upper(pregunta) like ?", search.toUpperCase()],
+		order: "pregunta ASC"
+	}).then(function(quizes){
 		res.render('quizes/index.ejs', {quizes: quizes});
 	}).catch (function(error){next(error);});
 };
