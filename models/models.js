ad9c033a0788c3a42c876705368a2eb72a6dde5a
Sixto Solís <sixtosentido@gmail.com>
2015-06-21 14:44:17 +0200

    Preguntas múltiples

--- models/models.js
+++ models/models.js
@@ -41,15 +41,19 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en BBDD
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
 	//success ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(count){
+	Quiz.count().then(function(count){
 		if (count===0){ //La tabla solo se inicializa si está vacía
 			Quiz.create({
 							pregunta: 'Capital de Italia',
 							respuesta: 'Roma'
+						});
+			Quiz.create({
+							pregunta: 'Capital de Portugal',
+							respuesta: 'Lisboa'
 						})
-			.success(function(){console.log('Base de datos inicializada')})
+			.then(function(){console.log('Base de datos inicializada')})
 		}
 		else console.log('Base de datos existente');
 	});
