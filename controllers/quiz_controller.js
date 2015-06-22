0afc64225cb2b8ab4ed85554d4e0b5f04fd62aae
Sixto Solís <sixtosentido@gmail.com>
2015-06-22 21:05:50 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -91,3 +91,10 @@ exports.update = function (req,res){
 		}
 	});
 };
+
+// DELETE /quizes/:quizId(\\d+)
+exports.destroy = function(req, res){
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+	}).catch(function(error){next(error)});
+};
