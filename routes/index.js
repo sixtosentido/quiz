ad8879b7a1cb2943ba292001d4093674940850cb
Sixto Solís <sixtosentido@gmail.com>
2015-06-22 20:35:39 +0200

    Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,7 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/author', quizController.author);
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
 
 module.exports = router;
