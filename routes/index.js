1f9c2028e8029869a8f2dfd34dc784e4b988f620
Sixto Solís <sixtosentido@gmail.com>
2015-06-29 06:06:33 +0200

    Control de sesiones

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,10 @@ router.get('/', function(req, res) {
 
 //Autoload de rutas que tienen :quizId
 router.param('quizId', quizController.load);
+
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
